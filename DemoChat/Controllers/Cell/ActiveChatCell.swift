8a9d57a2a19e2e40f8847700b911b39f7367205a
Nikola Kharkevich <niko.kharkevych@gmail.com>
2022-01-23 19:55:48 +0200

    customize ActiveChatCell

---
+++ DemoChat/Controllers/Cell/ActiveChatCell.swift
@@ -0,0 +1,109 @@
+//
+//  ActiveChatCell.swift
+//  DemoChat
+//
+//  Created by Nikola Kharkevich on 23.01.2022.
+//
+
+import UIKit
+
+protocol SelfConfiguringCell {
+    static var reuseId: String { get }
+    func configure(with value: MChat)
+}
+
+class ActiveChatCell: UICollectionViewCell, SelfConfiguringCell {
+    
+    static var reuseId: String = "ActiveChatCell"
+    let friendImageView = UIImageView()
+    let friendName = UILabel(text: "User name", font: .laoSangamMN20())
+    let lastMessage = UILabel(text: "How are you?", font: .laoSangamMN18())
+    let gradientVIew = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupConstraints()
+        backgroundColor = .white
+        
+        // скругляем углы ячейки
+        self.layer.cornerRadius = 4
+        self.clipsToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(with value: MChat) {
+        friendImageView.image = UIImage(named: value.userImageString)
+        friendName.text = value.userName
+        lastMessage.text = value.lastMessage
+    }
+}
+
+
+// MARK: - Setup Constraints
+extension ActiveChatCell {
+    private func setupConstraints() {
+        friendImageView.translatesAutoresizingMaskIntoConstraints = false
+        friendName.translatesAutoresizingMaskIntoConstraints = false
+        lastMessage.translatesAutoresizingMaskIntoConstraints = false
+        gradientVIew.translatesAutoresizingMaskIntoConstraints = false
+        
+        friendImageView.backgroundColor = .orange
+        gradientVIew.backgroundColor = .black
+        
+        addSubview(friendImageView)
+        addSubview(friendName)
+        addSubview(lastMessage)
+        addSubview(gradientVIew)
+        
+        NSLayoutConstraint.activate([
+            friendImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            friendImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            friendImageView.heightAnchor.constraint(equalToConstant: 78),
+            friendImageView.widthAnchor.constraint(equalToConstant: 78)
+        ])
+        
+        NSLayoutConstraint.activate([
+            friendName.topAnchor.constraint(equalTo: self.topAnchor, constant: 12),
+            friendName.leadingAnchor.constraint(equalTo: friendImageView.trailingAnchor, constant: 16),
+            friendName.trailingAnchor.constraint(equalTo: gradientVIew.leadingAnchor, constant: 16)
+        ])
+        
+        NSLayoutConstraint.activate([
+           lastMessage.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -14),
+           lastMessage.leadingAnchor.constraint(equalTo: friendImageView.trailingAnchor, constant: 16),
+           lastMessage.trailingAnchor.constraint(equalTo: gradientVIew.leadingAnchor, constant: 16)
+        ])
+        
+        NSLayoutConstraint.activate([
+            gradientVIew.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            gradientVIew.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            gradientVIew.heightAnchor.constraint(equalToConstant: 78),
+            gradientVIew.widthAnchor.constraint(equalToConstant: 8)
+        ])
+    }
+}
+
+// MARK: - SwiftUI
+// Чтобы пользоваться режимом canvas
+import SwiftUI
+
+struct ActiveChatCellProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let viewController = MainTabBarController()
+        
+        func makeUIViewController(context: Context) -> some UIViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        }
+    }
+}
--- DemoChat/Controllers/ListViewController.swift
+++ DemoChat/Controllers/ListViewController.swift
@@ -61,8 +61,7 @@ class ListViewController: UIViewController {
         collectionView.backgroundColor = .mainWhite()
         view.addSubview(collectionView)
         
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellid")
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellid2")
+        collectionView.register(ActiveChatCell.self, forCellWithReuseIdentifier: ActiveChatCell.reuseId)
     }
     
     private func reloadData() {
@@ -85,15 +84,19 @@ extension ListViewController {
             }
             switch section {
             case .waitingChats:
-                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid2", for: indexPath)
-                cell.backgroundColor = .red
-                return cell
+                return self.configure(cellType: ActiveChatCell.self, with: chat, for: indexPath)
             case .activeChats:
-                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
-                cell.backgroundColor = .blue
-                return cell
+                return self.configure(cellType: ActiveChatCell.self, with: chat, for: indexPath)
             }
         })
+    }
+    
+    private func configure<T: SelfConfiguringCell>(cellType: T.Type, with value: MChat, for indexPath: IndexPath) -> T {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellType.reuseId, for: indexPath) as? T else {
+            fatalError("Unable to dequeue\(cellType)")
+        }
+        cell.configure(with: value)
+        return cell
     }
 }
 
